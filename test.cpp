44612e700977088b2d805b5258bfa93d657d0d62
Qiaomeng Qin <qinqiaomeng@outlook.com>
2022-10-28 19:35:04 -0400

    calculate Rwg

--- test.cpp
+++ test.cpp
@@ -197,9 +197,9 @@ namespace IMU {
             // Sophus/Eigen
             mTbc = sophTbc;
             mTcb = mTbc.inverse();
-            // 噪声协方差 ?
+            // 噪声协方差 TODO
             Cov.diagonal() << ng2, ng2, ng2, na2, na2, na2;
-            // 随机游走协方差 ?
+            // 随机游走协方差 TODO
             CovWalk.diagonal() << ngw2, ngw2, ngw2, naw2, naw2, naw2;
         }
 
@@ -518,20 +518,20 @@ namespace IMU {
 //            return dP + JPg * db.head(3) + JPa * db.tail(3);
 //        }
 //
-//        Eigen::Matrix3f GetOriginalDeltaRotation() {
-//            std::unique_lock<std::mutex> lock(mMutex);
-//            return dR;
-//        }
-//
-//        Eigen::Vector3f GetOriginalDeltaVelocity() {
-//            std::unique_lock<std::mutex> lock(mMutex);
-//            return dV;
-//        }
-//
-//        Eigen::Vector3f GetOriginalDeltaPosition() {
-//            std::unique_lock<std::mutex> lock(mMutex);
-//            return dP;
-//        }
+        Eigen::Matrix3f GetOriginalDeltaRotation() {
+            std::unique_lock<std::mutex> lock(mMutex);
+            return dR;
+        }
+
+        Eigen::Vector3f GetOriginalDeltaVelocity() {
+            std::unique_lock<std::mutex> lock(mMutex);
+            return dV;
+        }
+
+        Eigen::Vector3f GetOriginalDeltaPosition() {
+            std::unique_lock<std::mutex> lock(mMutex);
+            return dP;
+        }
 //
 //        Bias GetOriginalBias() {
 //            std::unique_lock<std::mutex> lock(mMutex);
@@ -595,7 +595,8 @@ namespace IMU {
 
             integrable() {}
 
-            integrable(Eigen::Vector3f a_, Eigen::Vector3f w_, const float &t_) : a(std::move(a_)), w(std::move(w_)), t(t_) {}
+            integrable(Eigen::Vector3f a_, Eigen::Vector3f w_, const float &t_) : a(std::move(a_)), w(std::move(w_)),
+                                                                                  t(t_) {}
 
             Eigen::Vector3f a, w;
             float t;
@@ -609,6 +610,8 @@ namespace IMU {
 
 int main() {
 
+    clock_t start_time, end_time;
+
     // Guassian noise
     float noise_mean_real = 0;
     float noise_standard_deviation_real = 0.05;
@@ -619,8 +622,16 @@ int main() {
     Eigen::Vector3f a, a_real, w, w_real;
     std::vector<Eigen::Matrix<float, 6, 1>> all_measurements;
 
+    // IMU初始化的开始条件
     float min_time = 1;
     int min_KF = 10;
+
+    // IMU bias and calibration
+    IMU::Bias b(0, 0, 0, 0, 0, 0);
+    Sophus::SE3<float> Tbc(Eigen::Matrix3f::Identity(), Eigen::Vector3f(0, 0, 0));
+    IMU::Calib c(Tbc, 0.04, 0.04, 0.01, 0.01);
+
+    auto *preintegrated = new IMU::Preintegrated(b, c);
 
     // 重力系到世界系的旋转矩阵
     Eigen::Matrix3f Rwg;
@@ -629,8 +640,10 @@ int main() {
     dirG.setZero();
 
     // main loop
+    int have_imu_num = 0;
     float t = 0;
-    while (t < 1000) {
+    end_time = clock();
+    while (t < 3) { // 3s
         // Generate noise
         noise << n(e), n(e), n(e), n(e), n(e), n(e);
 //        std::cout << "noise: " << std::endl << noise << std::endl;
@@ -639,10 +652,48 @@ int main() {
         w_real << sin(t), cos(t), sin(t);
         a << sin(t) + noise[0], cos(t) + noise[1], cos(t) + noise[2];
         w << sin(t) + noise[3], cos(t) + noise[4], sin(t) + noise[5];
-        std::cout << "imu_measurement: " << std::endl << a << std::endl << w << std::endl;
+//        Eigen::Matrix<float, 6, 1> meas;
+//        meas << a, w;
+//        std::cout << "imu_measurement: " << std::endl << a << std::endl << w << std::endl;
+//        all_measurements.push_back(meas);
+        // Get system time
+        start_time = clock();
+        preintegrated->IntegrateNewMeasurement(a, w, (float) (start_time - end_time) / CLOCKS_PER_SEC);
+        std::cout << "dR： " << std::endl << preintegrated->GetOriginalDeltaRotation() << std::endl;
+        t = t + (float) (start_time - end_time) / CLOCKS_PER_SEC;
+        std::cout << "t: " << t << std::endl;
 
+        have_imu_num++;
+        // 初始化时关于速度的预积分定义Ri.t()*(s*Vj - s*Vi - Rwg*g*tij)
+        dirG -= preintegrated->GetOriginalDeltaRotation() * preintegrated->GetOriginalDeltaVelocity();
+        // 求取实际的速度，位移/时间
+        Eigen::Vector3f _vel =
+                preintegrated->GetOriginalDeltaPosition() / preintegrated->dT;
+        if (have_imu_num < 6) {
+            std::cout << "imu初始化失败, 由于带有imu预积分信息的关键帧数量太少" << std::endl;
+        } else {
+            // dirG = sV1 - sVn + n*Rwg*g*t
+            // 归一化，约等于重力在世界坐标系下的方向
+            dirG = dirG / dirG.norm();
+            // 原本的重力方向
+            Eigen::Vector3f gI(0.0f, 0.0f, -1.0f);
+            // 求重力在世界坐标系下的方向与重力在重力坐标系下的方向的叉乘
+            Eigen::Vector3f v = gI.cross(dirG);
+            // 求叉乘模长
+            const float nv = v.norm();
+            // 求转角大小
+            const float cosg = gI.dot(dirG);
+            const float ang = acos(cosg);
+            // v/nv 表示垂直于两个向量的轴  ang 表示转的角度，组成角轴
+            Eigen::Vector3f vzg = v * ang / nv;
+            // 获得重力坐标系到世界坐标系的旋转矩阵的初值
+            Rwg = Sophus::SO3f::exp(vzg).matrix();
+            std::cout<< "Rwg: " << std::endl << Rwg << std::endl;
+//            mRwg = Rwg.cast<double>();
+//            mTinit = mpCurrentKeyFrame->mTimeStamp-mFirstTs;
+        }
 
-        t = t + 0.01;
+        end_time = clock();
     }
 
     return 0;
