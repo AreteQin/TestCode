bf51b067674cbfe89047b165cc4f6bb68efb6d22
qin <qinqiaomeng@outlook.com>
2024-01-06 21:06:01 -0500

    realsense subscribe test

--- src/ros_test_node.cpp
+++ src/ros_test_node.cpp
@@ -1,55 +1,60 @@
 //
-// Created by qin on 01/01/24.
+// Created by qin on 5/2/22.
 //
+
 #include <ros/ros.h>
-// PCL specific includes
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <tf/transform_listener.h>
+#include <sophus/se3.hpp>
 
-#include <pcl/filters/voxel_grid.h>
+// sync sensor data
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
 
-ros::Publisher pub;
+// sync image
+#include <image_transport/subscriber_filter.h>
 
-void
-cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
-{
-    // Container for original & filtered data
-    pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2;
-    pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-    pcl::PCLPointCloud2 cloud_filtered;
-
-    // Convert to PCL data type
-    pcl_conversions::toPCL(*cloud_msg, *cloud);
-
-    // Perform the actual filtering
-    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-    sor.setInputCloud (cloudPtr);
-    sor.setLeafSize (0.1, 0.1, 0.1);
-    sor.filter (cloud_filtered);
-
-    // Convert to ROS data type
-    sensor_msgs::PointCloud2 output;
-    pcl_conversions::moveFromPCL(cloud_filtered, output);
-
-    // Publish the data
-    pub.publish (output);
+void Callback(const sensor_msgs::ImageConstPtr &color, const sensor_msgs::ImageConstPtr &depth) {
+    try {
+        cv::imshow("camera/depth/image_raw", cv_bridge::toCvShare(depth, "mono16")->image);
+        cv::waitKey(1);
+    }
+    catch (cv_bridge::Exception &e) {
+        ROS_ERROR("Could not convert from '%s' to 'mono8'.", depth->encoding.c_str());
+    }
+    try {
+        cv::imshow("camera/color/image_raw", cv_bridge::toCvShare(color, "rgb8")->image);
+        cv::waitKey(1);
+    }
+    catch (cv_bridge::Exception &e) {
+        ROS_ERROR("Could not convert from '%s' to 'rgb8'.", color->encoding.c_str());
+    }
 }
 
-int
-main (int argc, char** argv)
-{
-    // Initialize ROS
-    ros::init (argc, argv, "my_pcl_tutorial");
-    ros::NodeHandle nh;
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "pose_subscriber");
+    ros::NodeHandle nh_loop_detector;
 
-    // Create a ROS subscriber for the input point cloud
-    ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("input", 1, cloud_cb);
+    cv::namedWindow("camera/color/image_raw");
+    cv::namedWindow("camera/depth/image_raw");
+    image_transport::ImageTransport it(nh_loop_detector);
+    image_transport::SubscriberFilter sub_color(it, "camera/color/image_raw", 1);
+    image_transport::SubscriberFilter sub_depth(it, "camera/depth/image_rect_raw", 1);
 
-    // Create a ROS publisher for the output point cloud
-    pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+    // Sync the subscribed data
+    message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image>
+            sync(sub_color, sub_depth, 10);
+    //所有subscriber使用一个回调函数，_1至_4分别是callback的四个输入参数
+    // All subscribers use one callback function, _1 to _4 is the input variables
+    sync.registerCallback(boost::bind(&Callback, _1, _2));
 
-    // Spin
-    ros::spin ();
+    ros::Rate rate(30.0);
+    while (nh_loop_detector.ok()) {
+        ros::spinOnce();
+        rate.sleep();
+    }
 }
