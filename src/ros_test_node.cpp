419eaa8d5091dfdbf5b16e0adce4a82e91e4ee0d
qin <qinqiaomeng@outlook.com>
2024-04-16 12:36:34 -0400

    extra images from rosbag

--- src/ros_test_node.cpp
+++ src/ros_test_node.cpp
@@ -1,60 +1,63 @@
-//
-// Created by qin on 5/2/22.
-//
-
 #include <ros/ros.h>
-#include <image_transport/image_transport.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
 #include <opencv2/highgui/highgui.hpp>
-#include <iostream>
-#include <tf/transform_listener.h>
-#include <sophus/se3.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
-// sync sensor data
-#include <message_filters/subscriber.h>
-#include <message_filters/time_synchronizer.h>
+void extractImagesFromBag(const std::string& bag_file, const std::string& output_dir, const std::string& image_topic)
+{
+    rosbag::Bag bag;
+    try
+    {
+        bag.open(bag_file, rosbag::bagmode::Read);
+        rosbag::View view(bag, rosbag::TopicQuery(image_topic));
 
-// sync image
-#include <image_transport/subscriber_filter.h>
+        cv_bridge::CvImagePtr cv_ptr;
+        int frame_number = 0;
 
-void Callback(const sensor_msgs::ImageConstPtr &color, const sensor_msgs::ImageConstPtr &depth) {
-    try {
-        cv::imshow("camera/depth/image_raw", cv_bridge::toCvShare(depth, "mono16")->image);
-        cv::waitKey(1);
+        for (const rosbag::MessageInstance& msg : view)
+        {
+            sensor_msgs::Image::ConstPtr image_msg = msg.instantiate<sensor_msgs::Image>();
+            if (image_msg)
+            {
+                try
+                {
+                    cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
+                    std::string image_filename = output_dir + "/frame" + std::to_string(frame_number) + ".jpg";
+                    cv::imwrite(image_filename, cv_ptr->image);
+                    frame_number++;
+                }
+                catch (cv_bridge::Exception& e)
+                {
+                    ROS_ERROR("Error converting image: %s", e.what());
+                }
+            }
+        }
     }
-    catch (cv_bridge::Exception &e) {
-        ROS_ERROR("Could not convert from '%s' to 'mono8'.", depth->encoding.c_str());
+    catch (rosbag::BagException& e)
+    {
+        ROS_ERROR("Error opening bag file: %s", e.what());
     }
-    try {
-        cv::imshow("camera/color/image_raw", cv_bridge::toCvShare(color, "rgb8")->image);
-        cv::waitKey(1);
-    }
-    catch (cv_bridge::Exception &e) {
-        ROS_ERROR("Could not convert from '%s' to 'rgb8'.", color->encoding.c_str());
-    }
+    bag.close();
 }
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "pose_subscriber");
-    ros::NodeHandle nh_loop_detector;
-
-    cv::namedWindow("camera/color/image_raw");
-    cv::namedWindow("camera/depth/image_raw");
-    image_transport::ImageTransport it(nh_loop_detector);
-    image_transport::SubscriberFilter sub_color(it, "camera/color/image_raw", 1);
-    image_transport::SubscriberFilter sub_depth(it, "camera/depth/image_rect_raw", 1);
-
-    // Sync the subscribed data
-    message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image>
-            sync(sub_color, sub_depth, 10);
-    //所有subscriber使用一个回调函数，_1至_4分别是callback的四个输入参数
-    // All subscribers use one callback function, _1 to _4 is the input variables
-    sync.registerCallback(boost::bind(&Callback, _1, _2));
-
-    ros::Rate rate(30.0);
-    while (nh_loop_detector.ok()) {
-        ros::spinOnce();
-        rate.sleep();
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "rosbag_image_extractor");
+    if (argc != 4)
+    {
+        ROS_ERROR("Usage: %s <bag_file> <output_directory> <image_topic>", argv[0]);
+        return 1;
     }
+
+    std::string bag_file = argv[1];
+    std::string output_dir = argv[2];
+    std::string image_topic = argv[3];
+
+    extractImagesFromBag(bag_file, output_dir, image_topic);
+
+    ROS_INFO("Images extracted successfully!");
+    return 0;
 }
