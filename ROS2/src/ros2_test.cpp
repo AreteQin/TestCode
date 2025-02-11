2a62178c235fcfd6554ab8c10103d66ba79610a2
qin <qinqiaomeng@outlook.com>
2025-02-11 10:26:34 -0500

    created ros2

--- ROS2/src/ros2_test.cpp
+++ ROS2/src/ros2_test.cpp
@@ -1,19 +1,31 @@
 #include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/image.hpp>
-#include <cv_bridge/cv_bridge.h>
-#include <image_transport/image_transport.hpp>
-#include <opencv2/opencv.hpp>
-#include <map>
 
-#include <dji_liveview.h>
-#include <dji_logger.h>
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("my_node"); // Create your node
 
-#include "application.hpp"
+    // Create a callback group
+    rclcpp::CallbackGroup::SharedPtr callback_group = rclcpp::create_callback_group();
 
-int main(int argc, char **argv) {
-    Application application(argc, argv);
-    char inputChar;
-    T_DjiOsalHandler *osalHandler = DjiPlatform_GetOsalHandler();
-    T_DjiReturnCode returnCode;
+    // Add a subscription (or timer, service, etc.) to the callback group
+    auto subscription = node->create_subscription<std_msgs::msg::String>(
+      "topic_name", 10,
+      [&](std_msgs::msg::String::SharedPtr msg) {
+        RCLCPP_INFO(node->get_logger(), "I heard: '%s'", msg->data.c_str());
+      },
+      rclcpp::SubscriptionOptions(rclcpp::QoS(10), callback_group) // Associate with the group
+    );
+
+    // Create the executor
+    rclcpp::executors::SingleThreadedExecutor executor;
+
+    // Add the node to the executor (not the callback group)
+    executor.add_node(node);
+
+    // Spin the executor
+    executor.spin();
+
+    rclcpp::shutdown();
     return 0;
 }
